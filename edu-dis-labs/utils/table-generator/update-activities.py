d54cac21aceb28dec5fc9b6b582262ce3131fc10
victoriavodyana <145166568+victoriavodyana@users.noreply.github.com>
2024-05-26 20:06:32 +0300

    Delete edu-dis-labs directory

--- edu-dis-labs/utils/table-generator/update-activities.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import updater
-
-target_folder = "activities"
-converter_args = "-a -nv"
-
-updater.process(target_folder, converter_args)
--- edu-dis-labs/utils/table-generator/update-tables.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import updater
-
-target_folder = "tables"
-converter_args = "-nv"
-
-updater.process(target_folder, converter_args)
